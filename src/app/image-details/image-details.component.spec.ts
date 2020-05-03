aae8dae00cd81fd8409ae1dfad8ebbdee5f30717
Thomas Gil N <thomasgil@process-on-line.com>
2020-05-03 18:18:50 -0500

    gallery component test realizadas

--- src/app/image-details/image-details.component.spec.ts
+++ src/app/image-details/image-details.component.spec.ts
@@ -1,29 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageDetailComponent } from './image-details.component';
+import { ImageService } from '../image.service';
+import { ActivatedRoute } from '@angular/router';
 
-xdescribe('ImageDetailsComponent', () => {
+describe('ImageDetailsComponent', () => {
   let component: ImageDetailComponent;
   let fixture: ComponentFixture<ImageDetailComponent>;
+  let imageServiceSpy: ImageService;
+  let route = {snapshot: {params:{ id: 1}}};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImageDetailComponent ]
+      declarations: [ ImageDetailComponent ],
+      providers: [
+        ImageService,
+        { provide: ActivatedRoute, useValue: route}
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ImageDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    imageServiceSpy = TestBed.inject(ImageService);
   });
 
   it('should create', () => {
+    route.snapshot.params.id = 1;
+    fixture = TestBed.createComponent(ImageDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
-  describe('#ngOnInit', ()=>{
-    
+  it('Si no hay un parametro en la ruta debe fallar la creacion del componente', () =>{
+    route.snapshot.params.id = null;
+    fixture = TestBed.createComponent(ImageDetailComponent);
+    component = fixture.componentInstance;
+    expect(function() { try{fixture.detectChanges()} catch(e){ throw new TypeError()}}).toThrow( new TypeError());
+  });
+
+  it('Si el id del parametro no corresponde a una imagen debe fallar la crearcion del componente', () =>{
+    route.snapshot.params.id = -1;
+    fixture = TestBed.createComponent(ImageDetailComponent);
+    component = fixture.componentInstance;
+    expect(function() { try{fixture.detectChanges()} catch(e){ throw new TypeError()}}).toThrow( new TypeError());
   });
 });
--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,19 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { GalleryComponent } from './image-gallery.component';
+import { ImageService } from '../image.service';
+import { FilterimagesPipe } from '../filterimages.pipe';
 
 xdescribe('ImageGalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
+  let imageServiceSpy: ImageService = jasmine.createSpyObj('ImageService',['getImage','getImages']);
+  let filterimagesPipe: FilterimagesPipe = new FilterimagesPipe();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
+      declarations: [ GalleryComponent ],
+      providers: [
+        {provide: ImageService, useValue: imageServiceSpy},
+        {provide: FilterimagesPipe, useValue: filterimagesPipe}
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    imageServiceSpy = TestBed.inject(ImageService);
+    filterimagesPipe = TestBed.inject(FilterimagesPipe);
     fixture = TestBed.createComponent(GalleryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
